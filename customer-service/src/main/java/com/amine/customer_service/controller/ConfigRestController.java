a336ad76173f2113968c1cb055c0ac8e810d0375
amine kabli <a_kabli@etu.enset-media.ac.ma>
2024-12-24 22:49:47 +0100

    v1.1.5

--- customer-service/src/main/java/com/amine/customer_service/controller/ConfigRestController.java
+++ customer-service/src/main/java/com/amine/customer_service/controller/ConfigRestController.java
@@ -1,12 +1,14 @@
 package com.amine.customer_service.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
  import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigRestController {
     @Value("${global.params.p1}")
     private String p1;
