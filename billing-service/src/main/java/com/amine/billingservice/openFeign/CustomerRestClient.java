085ead78c57e2c129cb8f137a27c084f15d5b3dc
aminekabli99 <a_kabli@etu.enset-media.ac.ma>
2024-12-26 16:57:42 +0100

    1.1.13

--- billing-service/src/main/java/com/amine/billingservice/openFeign/CustomerRestClient.java
+++ billing-service/src/main/java/com/amine/billingservice/openFeign/CustomerRestClient.java
@@ -1,6 +1,7 @@
 package com.amine.billingservice.openFeign;
 
 import com.amine.billingservice.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,22 @@ import java.util.List;
 @FeignClient(name = "customer-service")
 public interface CustomerRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerServiceCB",fallbackMethod = "getDefaultCustomer")
     public Customer getCustomer(@PathVariable("id") Long id);
 
     @GetMapping("/customers")
     public PagedModel<Customer> getAllCustomers();
+
+
+    default Customer getDefaultCustomer(Long id,Exception exception) {
+        return Customer.builder()
+                .id(id)
+                .name("amineDF")
+                .email("amineDF@gmail.com")
+                .phone("123456789")
+                .build();
+    }
 }
+
+
+
